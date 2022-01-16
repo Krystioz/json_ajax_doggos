de8f535035b558258343f0515b4665032d235148
krystioz <gluszkokrystian@gmail.com>
2022-01-16 14:07:00 +0100

    dsplay 1 img when selecting breed and clicking for random

--- doggos.js
+++ doggos.js
@@ -22,12 +22,12 @@ fetch(BREEDS_URL)
 //any time someone selects some breed the function is called and
 //changes the value of API url
 
-select.addEventListener("change", function (event) {
-    //puts in the value in api of whatever breed is selected
-    let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
+// select.addEventListener("change", function (event) {
+//     //puts in the value in api of whatever breed is selected
+//     let url = `https://dog.ceo/api/breed/${event.target.value}/images/random`;
 
-    getDoggo(url);
-});
+//     getDoggo(url);
+// });
 
 //insert the url into image tag source
 
@@ -35,19 +35,19 @@ const img = document.querySelector(".dog-img");
 const spinner = document.querySelector(".spinner");
 const spinner2 = document.querySelector(".spinner2")
 
-function getDoggo(url) {
-    spinner.classList.add("show");
-    img.classList.remove("show");
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            img.src = data.message;
-            // spinner.classList.remove("show");
-            // img.classList.add("show");
-        });
-}
+// function getDoggo(url) {
+//     spinner.classList.add("show");
+//     img.classList.remove("show");
+//     fetch(url)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             img.src = data.message;
+//             // spinner.classList.remove("show");
+//             // img.classList.add("show");
+//         });
+// }
 function getDoggoRandom(url) {
     spinner2.classList.add("show");
     imgRandom.classList.remove("show");
